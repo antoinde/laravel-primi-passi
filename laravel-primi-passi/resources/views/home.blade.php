7af6c5d7061c2a9ac8b5b38b28f615e493eaae23
antoinde <antonello.indelicato98@gmail.com>
2022-10-30 20:38:30 +0100

    Aggiunto database con lista nomi da visualizzare

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,1 +1,33 @@
+<?php
+
+    $database=[
+        [
+            'nome' => 'Ronaldo',
+            'cognome' => 'Cristiano'
+        ],
+        [
+            'nome' => 'Antonio',
+            'cognome' => 'Cassano'
+        ],
+        [
+            'nome' => 'Federico',
+            'cognome' => 'Chiesa'
+        ],
+        [
+            'nome' => 'Lionel',
+            'cognome' => 'Messi'
+        ],
+        
+
+        ];
+?>
+
+
 <h1> hello world </h1>
+
+<?php
+
+    foreach($database as $utente)
+    echo "<div>" . $utente['nome'] . ' ' . $utente['cognome'] . "</div>";
+
+?>
